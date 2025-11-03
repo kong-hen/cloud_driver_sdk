3c868c244adaf505bf259a1a36db14e792a1a4b2
KongHen <430656781@qq.com>
2025-11-03 22:08:22 +0800

    优化：移除夸克网盘只支持50MB文件下载限制

--- Quark/session.py
+++ Quark/session.py
@@ -18,7 +18,7 @@ class QuarkSession:
             "referer": "https://pan.quark.cn",
             "origin": "https://pan.quark.cn",
             "pr": "ucpro",
-            "ua": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+            "ua": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) quark-cloud-drive/2.5.20 Chrome/100.0.4896.160 Electron/18.3.5.4-b478491100 Safari/537.36 Channel/pckk_other_ch",
         }
 
     def parse_cookie(
